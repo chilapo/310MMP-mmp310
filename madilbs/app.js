a69c8fabab974597b4d7fdf8147a8dcbe882e447
Daniel Munoz <daniel.munoz1@f1006-05-34128.bmcc.cuny.edu>
2018-02-20 19:06:19 -0500

    update

--- madilbs/app.js
+++ madilbs/app.js
@@ -19,18 +19,81 @@ submitButtom.onclick = function() {
         /*
             get all the words, generate  our story
         
-        */   
+        */  
     
-        const messsage = document.createElement("p");
-        messsage.textContent = exclamationInput.value
-                                +   "he said"
-                                +   adverbInput.value
-                                +   " as he jumped into his convertible "
-                                +   nounInput.value
-                                +   " and dorve off with his "
-                                +   adjectiveInput.value
-                                +   " wife "
-        document.body.appendChild(messsage);         
-            
+    const message = document.createElement("p");
+    document.body.appendChild(message);
+    
+    const message1 = document.createElement("p");
+    document.body.appendChild(message1);
+    
+       if (exclamationInput.value) {
+		message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+
+    }else {
+		message1.textContent = "You forgot to add an exclamation!";	
+	}
+    
+    
+    const message2 = document.createElement("p");
+	document.body.appendChild(message2);
+    
+     if (adverbInput.value) {
+		message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+	} else {
+		message2.textContent = "You forgot to add an adverb!";	
+	}
+    
+    const message3 = document.createElement("p");
+	document.body.appendChild(message3);
+    
+     if (nounInput.value) {
+		message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+	} else {
+		message3.textContent = "You forgot to add an noun!";	
+	}
+    
+    const message4 = document.createElement("p");
+	document.body.appendChild(message4);
+    const message5 = document.createElement("p");
+	document.body.appendChild(message5);
+     if (adjectiveInput.value) {
+		message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+	} else {
+		message4.textContent = "You forgot to add an adjective!";	
+	}
+     if  (adjectiveInput.value = exclamationInput.value){
+        message5.textContent = "Opss you cant write the same anwser";
+    }
+    
+    
 
 };
